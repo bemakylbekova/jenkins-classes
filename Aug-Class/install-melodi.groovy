21ed4925654931daa8544e63819640f8297de9e9
bemakylbekova <bemaangel49@gmail.com>
2020-10-31 20:48:19 +0000

    init

--- Aug-Class/install-melodi.groovy
+++ Aug-Class/install-melodi.groovy
@@ -4,15 +4,20 @@ node{
             string(defaultValue: '', description: 'Provide Remote Node IP', name: 'node_ip', trim: true)
         ])
     ])
-    stage("Git Pull"){
-        git url: 'https://github.com/ikambarov/ansible-melodi.git'
+    if(node_ip.length() > 7 ){
+        stage("Git Pull"){
+            git url: 'https://github.com/ikambarov/ansible-melodi.git'
+        }
+        withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-ssh-key', keyFileVariable: 'SSH_KEY', passphraseVariable: '', usernameVariable: 'SSH_USERNAME')]) {
+        stage("Ping"){
+                sh """
+                    export ANSIBLE_HOST_KEY_CHECKING=False
+                    ansible-playbook -i "${node_ip}," --private-key $SSH_KEY -u $SSH_USERNAME main.yml
+                """
+            } 
+        }  
     }
-    withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-ssh-key', keyFileVariable: 'SSH_KEY', passphraseVariable: '', usernameVariable: 'SSH_USERNAME')]) {
-       stage("Ping"){
-            sh """
-                export ANSIBLE_HOST_KEY_CHECKING=False
-                ansible-playbook -i "${node_ip}," --private-key $SSH_KEY -u $SSH_USERNAME main.yml
-            """
-        } 
-    }  
+    else {
+        error("Please provide a valid IP")
+    }
 }
